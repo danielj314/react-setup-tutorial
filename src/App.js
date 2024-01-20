9308703efc277cebdb836415810ae0b13a6c39e3
Daniel Jones <danieljones314@gmail.com>
2024-01-20 17:04:45 +0100

    Added image file loader capability to dev environment

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "App.css";
+import noimg from './noimage.png'; 
 
 
 const App = () => (
     <div>
         <h1 className="wow">Hello React!!!</h1>
+        <img src={noimg} alt="" />
     </div>
 );
 
